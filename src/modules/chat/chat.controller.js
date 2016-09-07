518b8c5962ac8a04652608dea89c9e9084289574
guuibayer <guiilherme.bayer@gmail.com>
2016-09-07 05:38:24 -0300

    Adicionado modulo de chat

---
+++ src/modules/chat/chat.controller.js
@@ -0,0 +1,11 @@
+(function() {
+  'use strict'
+  
+  angular
+    .module('ChatController', [])
+    .controller('ChatController', ChatController)
+
+    function ChatController() {
+        
+    }
+})()
